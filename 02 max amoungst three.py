48146ec478f6f20dac884cbb684f7d474653cc14
Ashwin Desai <107097239+ashwinvdesai@users.noreply.github.com>
2022-06-16 20:04:56 +0530

    To find out maximum of amoungust three given numbers

---
+++ 02 max amoungst three.py
@@ -0,0 +1,22 @@
+"""
+a = int(input('Enter a 1st Number: '))
+b = int(input('Enter a 2nd Number: '))
+c = int(input('Enter a 3rd Number: '))
+if (a > b) and (a > c):
+    print(a, ' is greater than ', b, '&', c)
+elif (b > a) and (b > c):
+    print(b, " is greater than ", a, '&', c)
+else:
+    print(c, ' is greater than ', a, '&', b)
+
+"""
+
+a = (input('Enter a 1st Number: '))
+b = (input('Enter a 2nd Number: '))
+c = (input('Enter a 3rd Number: '))
+if (a > b) and (a > c):
+    print(a, ' is greater than ', b, '&', c)
+elif (b > a) and (b > c):
+    print(b, " is greater than ", a, '&', c)
+else:
+    print(c, ' is greater than ', a, '&', b)
